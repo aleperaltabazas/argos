b24646ae30639795da5f789a0aa8c0ff8f1cf39a
Alejandro <aleperaltabazas@hotmail.com>
2021-02-22 23:14:30 -0300

    adding a merge function for two Argos

--- src/Data/Argos.hs
+++ src/Data/Argos.hs
@@ -1,6 +1,12 @@
 {-# LANGUAGE RecordWildCards #-}
 
-module Data.Argos where
+module Data.Argos
+  ( Argument(..)
+  , Argos
+  , spread
+  , merge
+  )
+where
 
 import Control.Monad (forM_)
 import Data.Map (Map)
@@ -19,11 +25,15 @@ data Argument
   , short :: Maybe Char
   } deriving (Show, Eq)
 
-spread :: Argument -> Map Int [String]
+type Argos = Map Int [String]
+
+spread :: Argument -> Argos
 spread arg = flip execState Map.empty $ go 0 arg
  where
-  go :: Int -> Argument -> State (Map Int [String]) ()
   go depth Option {..}  = modify (Map.insertManyAppended depth (("--" ++ long) : maybe [] (\s -> ['-' : [s]]) short))
   go depth Command {..} = do
     modify (Map.insertAppended depth name)
     forM_ arguments $ go (depth + 1)
+
+merge :: Argos -> Argos -> Argos
+merge xs ys = flip execState xs $ forM_ (Map.toList ys) $ \(depth, values) -> modify $ Map.insertManyAppended depth values
--- test/Data/ArgosSpec.hs
+++ test/Data/ArgosSpec.hs
@@ -8,36 +8,45 @@ import qualified Data.Map as Map
 import Test.Hspec
 
 spec :: Spec
-spec = describe "ArgosSpec" $ describe "spread" $ do
-  it "returns the long option with two dashes at the beginning" $ do
-    spread Option { long = "foo", short = Nothing } `shouldBe` Map.fromList [(0, ["--foo"])]
-    spread Option { long = "bar", short = Nothing } `shouldBe` Map.fromList [(0, ["--bar"])]
-  it "returns the long option with two dashes at the beginning and the short option with one" $ do
-    spread Option { long = "foo", short = Just 'f' } `shouldBe` Map.fromList [(0, ["--foo", "-f"])]
-    spread Option { long = "bar", short = Just 'b' } `shouldBe` Map.fromList [(0, ["--bar", "-b"])]
-  it "returns a map of length one" $ do
-    spread Command { name = "foo", arguments = [] } `shouldBe` Map.fromList [(0, ["foo"])]
-    spread Command { name = "bar", arguments = [] } `shouldBe` Map.fromList [(0, ["bar"])]
-  it "returns a map of length two" $ do
-    spread Command { name = "foo", arguments = [Option "bar" Nothing] } `shouldBe` Map.fromList [(0, ["foo"]), (1, ["--bar"])]
-    spread Command { name = "bar", arguments = [Command "foo" []] } `shouldBe` Map.fromList [(0, ["bar"]), (1, ["foo"])]
-    spread Command { name = "foo", arguments = [Option "bar" Nothing, Option "baz" Nothing] }
-      `shouldBe` Map.fromList [(0, ["foo"]), (1, ["--bar", "--baz"])]
-  it "returns a map of length four" $ do
-    let
-      c = Command
-        { name      = "foo"
-        , arguments =
-          [ Command
-            { name      = "bar"
-            , arguments =
-              [ Command { name = "baz", arguments = [] }
-              , Option { long = "biz", short = Nothing }
-              , Command { name = "qux", arguments = [Option { long = "corge", short = Nothing }] }
-              ]
-            }
-          , Command { name = "grault", arguments = [Option { long = "garply", short = Nothing }] }
-          ]
-        }
-    spread c
-      `shouldBe` Map.fromList [(0, ["foo"]), (1, ["bar", "grault"]), (2, ["baz", "--biz", "qux", "--garply"]), (3, ["--corge"])]
+spec = describe "ArgosSpec" $ do
+  describe "spread" $ do
+    it "returns the long option with two dashes at the beginning" $ do
+      spread Option { long = "foo", short = Nothing } `shouldBe` Map.fromList [(0, ["--foo"])]
+      spread Option { long = "bar", short = Nothing } `shouldBe` Map.fromList [(0, ["--bar"])]
+    it "returns the long option with two dashes at the beginning and the short option with one" $ do
+      spread Option { long = "foo", short = Just 'f' } `shouldBe` Map.fromList [(0, ["--foo", "-f"])]
+      spread Option { long = "bar", short = Just 'b' } `shouldBe` Map.fromList [(0, ["--bar", "-b"])]
+    it "returns a map of length one" $ do
+      spread Command { name = "foo", arguments = [] } `shouldBe` Map.fromList [(0, ["foo"])]
+      spread Command { name = "bar", arguments = [] } `shouldBe` Map.fromList [(0, ["bar"])]
+    it "returns a map of length two" $ do
+      spread Command { name = "foo", arguments = [Option "bar" Nothing] } `shouldBe` Map.fromList [(0, ["foo"]), (1, ["--bar"])]
+      spread Command { name = "bar", arguments = [Command "foo" []] } `shouldBe` Map.fromList [(0, ["bar"]), (1, ["foo"])]
+      spread Command { name = "foo", arguments = [Option "bar" Nothing, Option "baz" Nothing] }
+        `shouldBe` Map.fromList [(0, ["foo"]), (1, ["--bar", "--baz"])]
+    it "returns a map of length four" $ do
+      let
+        c = Command
+          { name      = "foo"
+          , arguments =
+            [ Command
+              { name      = "bar"
+              , arguments =
+                [ Command { name = "baz", arguments = [] }
+                , Option { long = "biz", short = Nothing }
+                , Command { name = "qux", arguments = [Option { long = "corge", short = Nothing }] }
+                ]
+              }
+            , Command { name = "grault", arguments = [Option { long = "garply", short = Nothing }] }
+            ]
+          }
+      spread c `shouldBe` Map.fromList
+        [(0, ["foo"]), (1, ["bar", "grault"]), (2, ["baz", "--biz", "qux", "--garply"]), (3, ["--corge"])]
+  describe "merge" $ do
+    it "is an empty map" $ merge Map.empty Map.empty `shouldBe` Map.empty
+    it "mixes both maps with no key merging"
+      $          merge (Map.fromList [(0, ["foo"]), (2, ["baz"])]) (Map.fromList [(1, ["bar"]), (3, ["biz"])])
+      `shouldBe` Map.fromList [(0, ["foo"]), (1, ["bar"]), (2, ["baz"]), (3, ["biz"])]
+    it "merges the values with the same key"
+      $ merge (Map.fromList [(0, ["foo"]), (1, ["bar"]), (2, ["qux", "quux"])]) (Map.fromList [(1, ["baz"]), (3, ["corge"])])
+      `shouldBe` Map.fromList [(0, ["foo"]), (1, ["bar", "baz"]), (2, ["qux", "quux"]), (3, ["corge"])]
